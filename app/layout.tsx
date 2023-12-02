f673d4b3a95ecae05de67866667694500f1492ae
orimaromcom <ori.marom10@gmail.com>
2023-12-02 18:28:40 +0700

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 import "./ui/globals.css";
 
 export const metadata = {
@@ -9,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="roboto">{children}</body>
     </html>
   );
 }
