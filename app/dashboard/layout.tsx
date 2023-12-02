3e50a32a739d7b70539317aa421a6d83098f3f9e
orimaromcom <ori.marom10@gmail.com>
2023-12-02 18:14:36 +0700

    wip changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
-import Navbar from "../ui/dashboard/navbar/navbar"
+import Navbar from "../ui/dashboard/navbar/navbar";
 import styles from "../ui/dashboard/dashboard.module.css";
 
 type Props = {
   children: JSX.Element;
 };
 
-const Layout = ({ children  } : Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.menu}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
-import "./ui/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./ui/globals.css";
 
 export const metadata = {
   title: "Ori Admin Dashboard",
@@ -11,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
